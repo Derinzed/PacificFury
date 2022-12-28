a76920302d9aad68760b27d11df98f1077ee2e2a
Derinzed <45581121+Derinzed@users.noreply.github.com>
2022-12-28 18:34:33 -0500

    Changelog 12/8/2022
    Mass Repo update for Kat integration

--- scripts/GamemodeTest.lua
+++ scripts/GamemodeTest.lua
@@ -2,5 +2,5 @@ MetalFuryFuncs.createShip("Fubuki",1000.0,0.0,0.0);
 MetalFuryFuncs.setScale(1,150.0);
 MetalFuryFuncs.createShip("Fletcher",-1000.0,0.0,0.0);
 MetalFuryFuncs.setScale(2,150.0);
-MetalFuryFuncs.createShip("Mogami",0.0,0.0,0.0);
-MetalFuryFuncs.setScale(3,1.0);
+--MetalFuryFuncs.createShip("Mogami",0.0,0.0,0.0);
+--MetalFuryFuncs.setScale(3,1.0);
